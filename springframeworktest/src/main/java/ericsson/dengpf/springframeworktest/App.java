5a225714d4d4c62675edbe9ea40c5aff0dfae656
Pengfei Deng <Pengfei.Deng@ericsson.com>
2015-07-21 13:54:24 +0800

    init

---
+++ springframeworktest/src/main/java/ericsson/dengpf/springframeworktest/App.java
@@ -0,0 +1,13 @@
+package ericsson.dengpf.springframeworktest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
